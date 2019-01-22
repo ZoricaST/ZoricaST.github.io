c38dec430f2a11b89d86e8f88ccf9fd6e986e461
Zorica Jelic <zoricagithub@gmail.com>
2019-01-23 00:46:24 +0100

    Add files via upload

---
+++ ponovoreact/build/precache-manifest.452223db9524018d94d21e84c3a733f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ponovoreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "acf88d470ab45fa0943a",
+    "url": "/ponovoreact/static/js/runtime~main.acf88d47.js"
+  },
+  {
+    "revision": "b33d2f7c527b72eaaa71",
+    "url": "/ponovoreact/static/js/main.b33d2f7c.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ponovoreact/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b33d2f7c527b72eaaa71",
+    "url": "/ponovoreact/static/css/main.ff0002d1.chunk.css"
+  },
+  {
+    "revision": "9627f005e378d3134677dc9b527dce4a",
+    "url": "/ponovoreact/index.html"
+  }
+];
