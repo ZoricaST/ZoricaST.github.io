07207c65f5822ed507c81061207de310f2822003
Zorica Jelic <zoricagithub@gmail.com>
2019-01-26 22:48:46 +0100

    Add files via upload

---
+++ treact/precache-manifest.47e5122f122dbda844c2ecda030bb5ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c86e2b90cc414b7a63",
+    "url": "/treact/static/js/runtime~main.22c86e2b.js"
+  },
+  {
+    "revision": "0defd92aef5c93f696dd",
+    "url": "/treact/static/js/main.0defd92a.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/treact/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "0defd92aef5c93f696dd",
+    "url": "/treact/static/css/main.4f5f6e40.chunk.css"
+  },
+  {
+    "revision": "8616dae0c7191a6abe3ca874cd47700f",
+    "url": "/treact/index.html"
+  }
+];
