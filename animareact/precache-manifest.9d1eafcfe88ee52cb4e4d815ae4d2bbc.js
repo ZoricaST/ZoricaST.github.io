3d72d7a8ff0e9318f6140d08b0f0ce3785e50660
Zorica Jelic <zoricagithub@gmail.com>
2019-02-02 15:15:33 +0100

    Add files via upload

---
+++ animareact/precache-manifest.9d1eafcfe88ee52cb4e4d815ae4d2bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c967369191448bbda52",
+    "url": "/animareact/static/js/runtime~main.5c967369.js"
+  },
+  {
+    "revision": "b9960884b039b0ac703c",
+    "url": "/animareact/static/js/main.b9960884.chunk.js"
+  },
+  {
+    "revision": "9001c06bb9c4de5afcca",
+    "url": "/animareact/static/js/1.9001c06b.chunk.js"
+  },
+  {
+    "revision": "b9960884b039b0ac703c",
+    "url": "/animareact/static/css/main.83b6b68b.chunk.css"
+  },
+  {
+    "revision": "3d77ad1e72ffbb66c20c2d13ab7161a6",
+    "url": "/animareact/index.html"
+  }
+];
